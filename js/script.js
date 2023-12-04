f6f746522dcb21b89586a6e79779bc8b92cc0897
Adin Izulhaq <adinizulhaq86@gmail.com>
2023-12-04 17:11:05 +0700

    mengupdate file website

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
 // Toggle hamburger menu
 const navbarExtra = document.querySelector(".navbar-extra");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
-const toggleClose1 = document.querySelector(".line-1");
-const toggleClose2 = document.querySelector(".line-2");
-const toggleClose3 = document.querySelector(".line-3");
 
 document.querySelector(".hamburger-menu").onclick = () => {
   navbarExtra.classList.toggle("active");
-  toggleClose1.classList.toggle("active");
-  toggleClose2.classList.toggle("active");
-  toggleClose3.classList.toggle("active");
+  hamburgerMenu.classList.toggle("toggle-close");
 };
 
 document.addEventListener("click", function (e) {
